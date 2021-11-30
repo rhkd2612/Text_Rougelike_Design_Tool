8a62b4c350f5c9a0cc3f1761cfe77e1663c07717
Lee SangBin(MUMOMU) <rhkd2612@naver.com>
2021-12-01 00:17:38 +0900

    2021-12-01 00:17 body make 1
    2021-12-01 00:17 body make 1
    BodyManager Scripting

--- TextRPGDesignTool/Assets/Resources/Scripts/Functions/BodyController.cs
+++ TextRPGDesignTool/Assets/Resources/Scripts/Functions/BodyController.cs
@@ -16,10 +16,37 @@ public class BodyController : MonoBehaviour
     public STATUS curStatus = STATUS.STAT;
 
     public GameObject[] popups;
+    public GameObject[] bodies;
+
+    public GameObject[] infoes;
+    public Transform[] infoPars;
+
+    private void Start()
+    {
+        GameObject gb = infoes[(int)curStatus];
+
+        foreach (var cur in JsonManager.Instance.statsDict)
+        {
+            GameObject newPref = Instantiate(gb, infoPars[(int)curStatus]);
+            newPref.transform.localScale = new Vector3(1, 1, 1);
+            newPref.transform.Find("Code").GetComponent<Text>().text = cur.Value.statCode;
+            newPref.transform.Find("Name").GetComponent<Text>().text = cur.Value.statName;
+            newPref.transform.Find("Explain").GetComponent<InputField>().text = cur.Value.statExplain;
+
+            Debug.Log(cur.Value.statExplain);
+            newPref.transform.Find("Toggle").GetComponent<Toggle>().isOn = cur.Value.isDefaultStat;
+            newPref.SetActive(true);
+        }
+    }
 
     public void SetStatus(int i)
     {
         curStatus = (STATUS)i;
+    }
+
+    public void ShowBodies()
+    {
+        bodies[(int)curStatus].SetActive(true);
     }
 
     public void CreateNewObject()
