aa0664728b80df673d6a9930cbaa95cf7f1f6f07
Lee SangBin(MUMOMU) <rhkd2612@naver.com>
2021-12-05 02:59:33 +0900

    2021-12-05 02:58 lots of bug
    stat info bug
    item info bug (stat degree can't delete in modifying

---
+++ TextRPGDesignTool/Assets/Resources/Scripts/Functions/ModifyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModifyController : MonoBehaviour
+{
+    public STATUS status;
+    string code;
+    SortedList<string,Info> myInfo;
+
+    void Start()
+    {
+        code = transform.Find("Code").GetComponent<Text>().text;
+        myInfo = JsonManager.Instance.sLists[(int)status];
+    }
+
+    public void ModifyName(string name)
+    {
+        myInfo[code].name = name;
+    }
+
+    public void ModifyExplain(string explain)
+    {
+        //myInfo[code].explain = explain;
+    }
+
+    public void ModifySetDefault(bool b)
+    {
+        //((Stat)myInfo[code]).isDefaultStat = b;
+    }
+}
